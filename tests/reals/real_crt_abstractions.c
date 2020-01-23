79f0bf7366583de609fa18ff3e1be21d29844f3c
Dan Cristoloveanu <dcristo@microsoft.com>
2020-01-22 16:12:35 -0800

    Add crt_abstractions and doublylinkedlist to the reals lib

--- tests/reals/real_crt_abstractions.c
+++ tests/reals/real_crt_abstractions.c
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define mallocAndStrcpy_s real_mallocAndStrcpy_s
-#define unsignedIntToString real_unsignedIntToString
-#define size_tToString real_size_tToString
+#include "real_crt_abstractions_renames.h"
 
 #define GBALLOC_H
 
