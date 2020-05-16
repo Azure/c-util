550d0aa5e7817a4dd0f112318f5513ac7685bf49
parth2199 <t-paagga@microsoft.com>
2020-05-15 20:05:45 -0400

    Finished win32.c, writing integration tests

--- adapters/interlocked_win32.c
+++ adapters/interlocked_win32.c
@@ -2,3 +2,158 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "azure_c_util/interlocked.h"
+#include "windows.h"
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_add, volatile int32_t*, addend, int32_t, value) 
+{
+	return (int32_t)InterlockedAdd((volatile LONG*)addend, (LONG)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_and, volatile int32_t*, destination, int32_t, value) 
+{
+	return (int32_t)InterlockedAnd((volatile LONG*)destination, (LONG)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_and_16, volatile int16_t*, destination, int16_t, value)
+{
+	return (int16_t)InterlockedAnd16((volatile SHORT*)destination, (SHORT)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_and_64, volatile int64_t*, destination, int64_t, value)
+{
+	return (int64_t)InterlockedAnd64((volatile LONG64*)destination, (LONG64)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int8_t, interlocked_and_8, volatile int8_t*, destination, int8_t, value)
+{
+	return (int8_t)InterlockedAnd8((volatile char*)destination, (char)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_compare_exchange, volatile int32_t*, destination, int32_t, exchange, int32_t, comperand)
+{
+	return (int32_t)InterlockedCompareExchange((volatile LONG*)destination, (LONG)exchange, (LONG)comperand);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, bool, interlocked_compare_exchange_128, volatile int64_t*, destination, int64_t, exchange_high, int64_t, exchange_low, int64_t*, comperand_result)
+{
+	return (bool)InterlockedCompareExchange128((volatile LONG64*)destination, (LONG64)exchange_high, (LONG64)exchange_low, (LONG64*)comperand_result);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_compare_exchange_16, volatile int16_t*, destination, int16_t, exchange, int16_t, comperand)
+{
+	return (int16_t)InterlockedCompareExchange16((volatile SHORT*)destination, (SHORT)exchange, (SHORT)comperand);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_compare_exchange_64, volatile int64_t*, destination, int64_t, exchange, int64_t, comperand)
+{
+	return (int64_t)InterlockedCompareExchange64((volatile LONG64*)destination, (LONG64)exchange, (LONG64)comperand);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, void*, interlocked_compare_exchange_pointer, volatile void**, destination, void*, exchange, void*, comperand)
+{
+	return (void*)InterlockedCompareExchange64((volatile PVOID*)destination, (PVOID)exchange, (PVOID)comperand);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_decrement, volatile int32_t*, addend)
+{
+	return (int32_t)InterlockedDecrement((volatile LONG*)addend);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_decrement_16, volatile int16_t*, addend)
+{
+	return (int16_t)InterlockedDecrement16((volatile SHORT*)addend);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_decrement_64, volatile int64_t*, addend)
+{
+	return (int64_t)InterlockedDecrement64((volatile LONG64*)addend);
+}
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_exchange, volatile int32_t*, target, int32_t, value)
+{
+	return (int32_t)InterlockedExchange((volatile LONG*)target, (LONG)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_exchange_16, volatile int16_t*, target, int16_t, value)
+{
+	return (int16_t)InterlockedExchange16((volatile SHORT*)target, (SHORT)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_exchange_64, volatile int64_t*, target, int64_t, value)
+{
+	return (int64_t)InterlockedExchange64((volatile LONG64*)target, (LONG64)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int8_t, interlocked_exchange_8, volatile int8_t*, target, int8_t, value)
+{
+	return (int8_t)InterlockedExchange8((volatile char*)target, (char)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_exchange_add, volatile int32_t*, addend, int32_t, value)
+{
+	return (int32_t)InterlockedExchangeAdd((volatile LONG*)addend, (LONG)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_exchange_add_64, volatile int64_t*, addend, int64_t, value)
+{
+	return (int64_t)InterlockedExchangeAdd64((volatile LONG64*)addend, (LONG64)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, void*, interlocked_exchange_pointer, volatile void**, target, void*, value)
+{
+	return (void*)InterlockedExchangeAdd((volatile PVOID*)target, (PVOID)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_increment, volatile int32_t*, addend)
+{
+	return (int32_t)InterlockedIncrement((volatile LONG*)addend);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_increment_16, volatile int16_t*, addend)
+{
+	return (int16_t)InterlockedIncrement16((volatile SHORT*)addend);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_increment_64, volatile int64_t*, addend)
+{
+	return (int64_t)InterlockedIncrement64((volatile LONG64*)addend);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_or, volatile int32_t*, destination, int32_t, value)
+{
+	return (int32_t)InterlockedOr((volatile LONG*)destination, (LONG)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_or_16, volatile int16_t*, destination, int16_t, value)
+{
+	return (int16_t)InterlockedOr16((volatile SHORT*)destination, (SHORT)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_or_64, volatile int64_t*, destination, int64_t, value)
+{
+	return (int64_t)InterlockedOr64((volatile LONG64*)destination, (LONG64)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int8_t, interlocked_or_8, volatile int8_t*, destination, int8_t, value)
+{
+	return (int8_t)InterlockedOr8((volatile char*)destination, (char)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int32_t, interlocked_xor, volatile int32_t*, destination, int32_t, value)
+{
+	return (int32_t)InterlockedXor((volatile LONG*)destination, (LONG)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int16_t, interlocked_xor_16, volatile int16_t*, destination, int16_t, value)
+{
+	return (int16_t)InterlockedXor16((volatile SHORT*)destination, (SHORT)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int64_t, interlocked_xor_64, volatile int64_t*, destination, int64_t, value)
+{
+	return (int64_t)InterlockedXor64((volatile LONG64*)destination, (LONG64)value);
+}
+
+IMPLEMENT_MOCKABLE_FUNCTION(, int8_t, interlocked_xor_8, volatile int8_t*, destination, int8_t, value)
+{
+	return (int8_t)InterlockedXor8((volatile char*)destination, (char)value);
+}
--- tests/interlocked_int/interlocked_int.c
+++ tests/interlocked_int/interlocked_int.c
@@ -17,10 +17,10 @@ static TEST_MUTEX_HANDLE g_testByTest;
 
 
 
-#include "azure_c_util/interlocked_if.h"
+#include "azure_c_util/interlocked.h"
 
 
-BEGIN_TEST_SUITE(interlocked_if_int)
+BEGIN_TEST_SUITE(interlocked_int)
 
 
 TEST_SUITE_INITIALIZE(a)
@@ -51,18 +51,195 @@ TEST_FUNCTION_CLEANUP(d)
     TEST_MUTEX_RELEASE(g_testByTest);
 }
 
-/*Tests_SRS_INTERLOCKED_IF_43_001 [`InterlockedIFAdd` shall perform an atomic addition operation on the specified `long` values.] */
-TEST_FUNCTION(InterlockedIFAdd_does_addition)
+/*
+Tests_SRS_INTERLOCKED_43_001 [ interlocked_add shall atomically add *addend with value and store the result in *addend.]
+Tests_SRS_INTERLOCKED_43_032: [interlocked_add shall return the result of the addition.]
+*/
+
+TEST_FUNCTION(interlocked_add_does_addition)
 {
     ///arrange
-    volatile long a = 1;
-    long b = 2;
+    volatile int32_t addend = INT32_MAX;
+    int32_t value = INT32_MIN;
 
     ///act
-    long c = InterlockedIFAdd(&a, b);
+    int32_t return_val = interlocked_add(&addend, value);
 
     ///assert
-    ASSERT_ARE_EQUAL(long, 3, c);
+    ASSERT_ARE_EQUAL(int32_t, -1, addend);
+    ASSERT_ARE_EQUAL(int32_t, -1, return_val);
 }
 
-END_TEST_SUITE(interlocked_if_int)
+/*
+Tests_SRS_INTERLOCKED_43_002[interlocked_and shall perform an atomic bitwise AND operation on the 32 - bit integer values * destination and value and store the result in * destination.]
+Tests_SRS_INTERLOCKED_43_033: [interlocked_and shall return the initial value of * destination.]
+*/
+
+TEST_FUNCTION(interlocked_and_does_bitwise_and)
+{
+    ///arrange
+    volatile uint32_t destination = 0xF0F0F0F0;
+    uint32_t value = 0x0F0F0F0F;
+
+    ///act
+    uint32_t return_val = (uint32_t)interlocked_and((volatile int32_t*)&destination, (int32_t)value);
+
+    ///assert
+    ASSERT_ARE_EQUAL(uint32_t, -1, destination);
+    ASSERT_ARE_EQUAL(uint32_t, 0xF0F0F0F0, return_val);
+}
+
+/*
+Tests_SRS_INTERLOCKED_43_003 [ interlocked_and_16 shall perform an atomic bitwise AND operation on the 16-bit integer values *destination and value and store the result in *destination.]
+Tests_SRS_INTERLOCKED_43_034: [ interlocked_and_16 shall return the initial value of *destination.]
+*/
+
+TEST_FUNCTION(interlocked_and_16_does_bitwise_and)
+{
+    ///arrange
+    volatile uint16_t destination = 0xF0F0;
+    uint16_t value = 0x0F0F;
+
+    ///act
+    uint16_t return_val = (uint16_t)interlocked_and_16((volatile int16_t*)&destination, (int16_t)value);
+
+
+    ///assert
+    ASSERT_ARE_EQUAL(uint16_t, -1, destination);
+    ASSERT_ARE_EQUAL(uint16_t, 0xF0F0, return_val);
+}
+
+/*
+SRS_INTERLOCKED_43_004 [ interlocked_and_64 shall perform an atomic bitwise AND operation on the 64-bit integer values *destination and value and store the result in *destination.]
+
+SRS_INTERLOCKED_43_035: [ interlocked_and_64 shall return the initial value of *destination.]
+*/
+
+TEST_FUNCTION(interlocked_and_64_does_bitwise_and)
+{
+    ///arrange
+    volatile uint64_t destination = 0xF0F0F0F0F0F0F0F0;
+    uint64_t value = 0x0F0F0F0F0F0F0F0F;
+
+    ///act
+    uint64_t return_val = (uint64_t)interlocked_and_64((volatile int64_t*)&destination, (int64_t)value);
+
+    ///assert
+    ASSERT_ARE_EQUAL(uint64_t, -1, destination);
+    ASSERT_ARE_EQUAL(uint64_t, 0xF0F0F0F0F0F0F0F0, return_val);
+}
+
+/*
+SRS_INTERLOCKED_43_005 [ interlocked_and_8 shall perform an atomic bitwise AND operation on the 8-bit integer values *destination and value and store the result in *destination]
+
+SRS_INTERLOCKED_43_036: [ interlocked_and_8 shall return the initial value of *destination. ]
+*/
+TEST_FUNCTION(interlocked_and_8_does_bitwise_and)
+{
+    ///arrange
+    volatile uint8_t destination = 0xF0;
+    uint8_t value = 0x0F0;
+
+    ///act
+    uint8_t return_val = (uint8_t)interlocked_and_8((volatile int8_t*)&destination, (int8_t)value);
+
+    ///assert
+    ASSERT_ARE_EQUAL(uint8_t, -1, destination);
+    ASSERT_ARE_EQUAL(uint8_t, 0xF0F0F0F0F0F0F0F0, return_val);
+}
+
+/*
+SRS_INTERLOCKED_43_006 [ interlocked_compare_exchange shall compare the 32-bit integers pointed to by destination and comperand. If they are equal, *destination is set to exchange. These operations are performed atomically.*]
+
+SRS_INTERLOCKED_43_037: [ interlocked_compare_exchange shall return the initial value of *destination.]
+*/
+
+TEST_FUNCTION(interlocked_compare_exchange_equal_case)
+{
+    ///arrange
+    volatile int32_t destination = INT32_MAX;
+    int32_t comperand = INT32_MAX;
+    int32_t exchange = INT32_MIN;
+
+    ///act
+    int32_t return_val = interlocked_compare_exchange(&destination, exchange, comperand);
+
+    ///assert
+    ASSERT_ARE_EQUAL(int8_t, INT32_MIN, destination);
+    ASSERT_ARE_EQUAL(int8_t, INT32_MAX, return_val);
+}
+/*
+SRS_INTERLOCKED_43_006 [ interlocked_compare_exchange shall compare the 32-bit integers pointed to by destination and comperand. If they are equal, *destination is set to exchange. These operations are performed atomically.*]
+
+SRS_INTERLOCKED_43_037: [ interlocked_compare_exchange shall return the initial value of *destination.]
+*/
+TEST_FUNCTION(interlocked_compare_exchange_not_equal_case)
+{
+    ///arrange
+    volatile int32_t destination = INT32_MAX;
+    int32_t comperand = INT32_MIN;
+    int32_t exchange = INT32_MIN;
+
+    ///act
+    int32_t return_val = interlocked_compare_exchange(&destination, exchange, comperand);
+
+    ///assert
+    ASSERT_ARE_EQUAL(int8_t, INT32_MAX, destination);
+    ASSERT_ARE_EQUAL(int8_t, INT32_MAX, return_val);
+}
+
+/*
+SRS_INTERLOCKED_43_007 [ interlocked_compare_exchange_128 shall compare *destination with *comperand_result. If they are equal, exchange_high and exchange_low are stored in the array specified by destination. These operations are performed atomically.]
+
+SRS_INTERLOCKED_43_039: [ interlocked_compare_exchange_128 shall store the initial value of *destination in *comperand_result regardless of the result of the comparison.` ]
+
+SRS_INTERLOCKED_43_038: [ interlocked_compare_exchange_128 shall return true if *comperand_result equals the original value of *destination.]
+
+*/
+
+TEST_FUNCTION(interlocked_compare_exchange_128_equal_case)
+{
+    ///arrange
+    volatile int64_t destination[2] = { INT64_MAX, INT64_MIN };
+    int64_t comperand_result[2] = { INT64_MAX, INT64_MIN };
+    int64_t exchange_high = 2;
+    int64_t exchange_low = 3;
+
+    ///act
+    bool return_val = interlocked_compare_exchange_128(destination, exchange_high, exchange_low, comperand_result);
+
+    ///assert
+    ASSERT_ARE_EQUAL(int64_t, 2, destination[0]);
+    ASSERT_ARE_EQUAL(int64_t, 3, destination[1]);
+    ASSERT_ARE_EQUAL(int64_t, INT32_MAX, comperand_result[0]);
+    ASSERT_ARE_EQUAL(int64_t, INT32_MIN, comperand_result[1]);
+    ASSERT_ARE_EQUAL(bool, true, return_val);
+}
+
+/*
+SRS_INTERLOCKED_43_039: [ interlocked_compare_exchange_128 shall store the initial value of *destination in *comperand_result regardless of the result of the comparison.` ]
+
+SRS_INTERLOCKED_43_063: [ interlocked_compare_exchange_128 shall return false if *comperand_result does not equal the original value of *destination. ]
+*/
+TEST_FUNCTION(interlocked_compare_exchange_128_not_equal_case)
+{
+    ///arrange
+    volatile int64_t destination[2] = { INT64_MAX, INT64_MIN };
+    int64_t comperand_result[2] = { INT64_MIN, INT64_MAX };
+    int64_t exchange_high = 2;
+    int64_t exchange_low = 3;
+
+    ///act
+    bool return_val = interlocked_compare_exchange_128(destination, exchange_high, exchange_low, comperand_result);
+
+    ///assert
+    ASSERT_ARE_EQUAL(int64_t, INT32_MAX, destination[0]);
+    ASSERT_ARE_EQUAL(int64_t, INT32_MIN, destination[1]);
+    ASSERT_ARE_EQUAL(int64_t, INT32_MAX, comperand_result[0]);
+    ASSERT_ARE_EQUAL(int64_t, INT32_MIN, comperand_result[1]);
+    ASSERT_ARE_EQUAL(bool, false, return_val);
+}
+
+
+
+END_TEST_SUITE(interlocked_int)
