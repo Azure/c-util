acb950af16135249fa4a16cbfd264c40cdc81a97
parth21999 <parth21999@gmail.com>
2020-06-10 17:25:02 -0400

    Removed adapters

--- tests/reals/real_string_utils.c
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#define GBALLOC_H
-
-#include "real_string_utils_renames.h"
-
-#include "../../adapters/string_utils.c"
