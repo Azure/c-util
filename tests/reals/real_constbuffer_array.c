2c0a5f173b94a55d41ea6698809e0fe3f3859ef9
Andrei Porumb <anporumb@microsoft.com>
2020-07-29 23:20:36 -0700

    reals etc

--- tests/reals/real_constbuffer_array.c
+++ tests/reals/real_constbuffer_array.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
+#include "real_interlocked_renames.h"
 #include "real_constbuffer_renames.h"
 #include "real_gballoc_hl_renames.h"
 
