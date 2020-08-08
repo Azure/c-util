317215be52cc454457e87825563485c6d7e444f0
Andrei Porumb <anporumb@microsoft.com>
2020-08-08 06:57:57 -0700

    it all compiles now

--- tests/constbuffer_ut/main.c
+++ tests/constbuffer_ut/main.c
@@ -3,6 +3,17 @@
 
 #include <stddef.h>
 #include "testrunnerswitcher.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "vld.h"
+
+#include "azure_c_pal/gballoc_hl.h"
+#include "azure_c_pal/gballoc_hl_redirect.h"
+
+#include "real_gballoc_hl.h"
+
+#include "mimalloc.h"
 
 int main(void)
 {
