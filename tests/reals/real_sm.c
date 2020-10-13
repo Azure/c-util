2bbadf7d211445e7e18cc449bd4d4d8f0f45cf00
Dan Cristoloveanu <dcristo@microsoft.com>
2020-10-12 20:35:55 -0700

    Fix reals to do renames for interlocked.h

--- tests/reals/real_sm.c
+++ tests/reals/real_sm.c
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 
+#include "real_interlocked_renames.h"
 #include "real_interlocked_hl_renames.h"
 #include "real_gballoc_hl_renames.h"
 
