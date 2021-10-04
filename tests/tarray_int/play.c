be72ad7f1fabe97fd57316fb3e5004af27d1c8d2
Andrei Porumb <anporumb@microsoft.com>
2021-10-04 14:54:31 -0700

    1 SHA to rule them all

---
+++ tests/tarray_int/play.c
@@ -0,0 +1,65 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <inttypes.h>
+
+#include "c_logging/xlogging.h"
+
+#include "c_util/thandle_ll.h"
+#include "c_util/tarray.h"
+#include "c_pal/gballoc_hl.h"
+#include "c_pal/gballoc_hl_redirect.h"
+
+#include "play_undo_op_types.h"
+#include "play_undo_op_tarray_types.h"
+
+#include "play.h"
+
+typedef struct PLAY_HANDLE_DATA_TAG
+{
+    uint32_t generation;
+    TARRAY(UNDO_OP) undo_op;
+}PLAY_HANDLE_DATA;
+
+PLAY_HANDLE play_create(uint32_t generation)
+{
+    PLAY_HANDLE play = malloc(sizeof(PLAY_HANDLE_DATA));
+    if (play == NULL)
+    {
+        LogError("failure in malloc(sizeof(PLAY_HANDLE_DATA)=%zu)",
+            sizeof(PLAY_HANDLE_DATA));
+    }
+    else
+    {
+        TARRAY(UNDO_OP) temp = TARRAY_CREATE(UNDO_OP)();
+        if (temp == NULL)
+        {
+            LogError("failure in TARRAY_CREATE(UNDO_OP)()");
+        }
+        else
+        {
+            TARRAY_INITIALIZE_MOVE(UNDO_OP)(&play->undo_op, &temp);
+            play->generation = generation;
+            goto allok;
+        }
+        free(play);
+        play = NULL;
+allok:;
+    }
+    return play;
+}
+
+void play_destroy(PLAY_HANDLE play)
+{
+    if (play == NULL)
+    {
+        LogError("invalid arg PLAY_HANDLE play=%p",
+            play);
+    }
+    else
+    {
+        TARRAY_ASSIGN(UNDO_OP)(&play->undo_op, NULL);
+        free(play);
+    }
+}
+
---
+++ tests/tarray_int/reals/real_tarray_undo_op.c
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdlib.h>
+
+#include "c_util/thandle_ll.h"
+#include "c_util/tarray_ll.h"
+
+#include "real_tarray_undo_op.h"
+
+#define THANDLE_MALLOC_FUNCTION malloc
+#define THANDLE_FREE_FUNCTION free
+
+THANDLE_LL_TYPE_DECLARE(TARRAY_TYPEDEF_NAME(real_UNDO_OP), TARRAY_TYPEDEF_NAME(UNDO_OP));
+THANDLE_LL_TYPE_DEFINE(TARRAY_TYPEDEF_NAME(real_UNDO_OP), TARRAY_TYPEDEF_NAME(UNDO_OP));
+
+TARRAY_LL_TYPE_DEFINE(real_UNDO_OP, UNDO_OP);
+
+
+
