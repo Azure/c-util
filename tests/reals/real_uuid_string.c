e637abb6fd1fe8a8f177fe244b0868b601baa3f5
Andrei Porumb <anporumb@microsoft.com>
2022-01-12 14:26:59 -0800

    fix uuid_string reals.c

--- tests/reals/real_uuid_string.c
+++ tests/reals/real_uuid_string.c
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "real_uuid_renames.h"
+#include "real_string_utils_renames.h"
 
 #include "real_uuid_string_renames.h" // IWYU pragma: keep
 
