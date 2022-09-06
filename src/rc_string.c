c4bd7248c9c1614918457ba56c0f9c5097ac7311
Bingyi Yang <bingyiyang@microsoft.com>
2022-09-06 13:33:57 -0700

    modify unit test

--- src/rc_string.c
+++ src/rc_string.c
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdlib.h>
+#include <stdarg.h>
 
 #include "macro_utils/macro_utils.h"
 
