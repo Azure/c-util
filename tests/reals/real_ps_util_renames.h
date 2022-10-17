78547c933dcda52b375ff87668eee0b5e9299b4e
Jelani Brandon <jebrando@microsoft.com>
2022-10-17 13:46:00 -0700

    chore: adding ps_util real file

---
+++ tests/reals/real_ps_util_renames.h
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define ps_util_terminate_process   real_ps_util_terminate_process
+#define ps_util_exit_process        real_ps_util_exit_process
