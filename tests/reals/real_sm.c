e45b7826352863d7405097e540c4f17daba8e35e
Jelani Brandon <jelani.brandon@microsoft.com>
2022-10-17 15:56:07 -0700

    chore: adding ps_util real file (#215)
    Co-authored-by: Jelani Brandon <jebrando@microsoft.com>

--- tests/reals/real_sm.c
+++ tests/reals/real_sm.c
@@ -6,6 +6,7 @@
 #include "real_gballoc_hl_renames.h" // IWYU pragma: keep
 #include "real_threadapi_renames.h" // IWYU pragma: keep
 #include "real_sync_renames.h" // IWYU pragma: keep
+#include "real_ps_util_renames.h" // IWYU pragma: keep
 #include "real_sm_renames.h" // IWYU pragma: keep
 
 #include "../../src/sm.c"
