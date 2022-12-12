f942475f5f6a501effbf23ae7cb6932bf140d88c
Jelani Brandon <jelani.brandon@microsoft.com>
2022-12-12 15:55:39 -0800

    Remove s_list from repo (#231)
    Co-authored-by: Jelani Brandon <jebrando@microsoft.com>

--- tests/reals/real_s_list.c
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#include "real_s_list_renames.h" // IWYU pragma: keep
-
-#include "s_list.c"
