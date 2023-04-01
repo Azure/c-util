cdf90ddee629b6abbbbe7e8a50ac87e6820234c8
David Robson <darobs@microsoft.com>
2023-03-31 19:21:24 -0700

    Remove SMHasher lib and include MurmurHash2 in c_util. (#269)
    * Remove SMHasher lib and include MurmurHash2 in c_util.
    
    * Keeping it reals.
    Use a function to get murmurhash2 location.
    Exit if we can't find murmurhas2 files.
    
    * traceability check removes all submodules, but we need smhasher to build now.
    
    * Add licensing info
    
    * Check current dir or parallel deps dir.

--- tests/hash_ut/hash_under_test.cpp
+++ tests/hash_ut/hash_under_test.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 // include Murmur hash with C signatures so that they can be nicely hooked with umock_c
 // this is due to the fact that the Murmur hash headers are not C/C++ compile friendly
---
+++ tests/reals/real_murmurhash2_renames.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define MurmurHash2 real_MurmurHash2
+#define MurmurHash64A real_MurmurHash64A
+#define MurmurHash64B real_MurmurHash64B
+#define MurmurHash2A real_MurmurHash2A
+#define MurmurHashNeutral2 real_MurmurHashNeutral2
+#define MurmurHashAligned2 real_MurmurHashAligned2
