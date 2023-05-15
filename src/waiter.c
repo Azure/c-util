221e43f66101d6605a3926ecc22b8c8d7453ac67
Parth Aggarwal <paaggarwal@microsoft.com>
2023-05-15 11:42:48 -0700

    removed sm

--- src/waiter.c
+++ src/waiter.c
@@ -21,7 +21,6 @@
 
 typedef struct WAITER_TAG
 {
-    SM_HANDLE sm;
     SRW_LOCK_HANDLE lock;
     THANDLE(RC_PTR) data;
     NOTIFICATION_CALLBACK notification_callback;
@@ -44,72 +43,29 @@ WAITER_HANDLE waiter_create(void)
     }
     else
     {
-        waiter->sm = sm_create("waiter");
-        if (waiter->sm == NULL)
+        waiter->lock = srw_lock_create("waiter", false);
+        if (waiter->lock == NULL)
         {
-            LogError("Failure in sm_create(\"waiter\")");
+            LogError("Failure in srw_lock_create(\"waiter\", false)");
             result = NULL;
         }
         else
         {
-            SM_RESULT sm_open_begin_result = sm_open_begin(waiter->sm);
-            if(sm_open_begin_result != SM_EXEC_GRANTED)
-            {
-                LogError("Failure in sm_open_begin(waiter->sm)");
-                result = NULL;
-            }
-            else
-            {
-                sm_open_end(waiter->sm, true);
-                waiter->lock = srw_lock_create("waiter", false);
-                if (waiter->lock == NULL)
-                {
-                    LogError("Failure in srw_lock_create(\"waiter\", false)");
-                    result = NULL;
-                }
-                else
-                {
-                    THANDLE_INITIALIZE(RC_PTR)(&waiter->data, NULL);
-                    waiter->notification_callback = NULL;
-                    waiter->notification_callback_context = NULL;
-                    waiter->notify_complete_callback = NULL;
-                    waiter->notify_complete_callback_context = NULL;
-                    waiter->registered = false;
-                    waiter->notified = false;
-                    waiter->callbacks_called = false;
-                    result = waiter;
-                    goto all_ok;
-                }
-            }
-            sm_destroy(waiter->sm);
+            THANDLE_INITIALIZE(RC_PTR)(&waiter->data, NULL);
+            waiter->notification_callback = NULL;
+            waiter->notification_callback_context = NULL;
+            waiter->notify_complete_callback = NULL;
+            waiter->notify_complete_callback_context = NULL;
+            waiter->registered = false;
+            waiter->notified = false;
+            waiter->callbacks_called = false;
+            result = waiter;
+            goto all_ok;
         }
         free(waiter);
     }
 all_ok:
     return result;
-}
-
-static void abandon(void* context)
-{
-    WAITER_HANDLE waiter = context;
-    srw_lock_acquire_exclusive(waiter->lock);
-    if (!waiter->callbacks_called)
-    {
-        sm_fault(waiter->sm);
-        if (waiter->registered)
-        {
-            waiter->notification_callback(waiter->notification_callback_context, NULL, WAITER_RESULT_ABANDONED);
-            sm_exec_end(waiter->sm);
-        }
-        if (waiter->notified)
-        {
-            waiter->notify_complete_callback(waiter->notify_complete_callback_context, WAITER_RESULT_ABANDONED);
-            THANDLE_ASSIGN(RC_PTR)(&waiter->data, NULL);
-            sm_exec_end(waiter->sm);
-        }
-        waiter->callbacks_called = true;
-    }
-    srw_lock_release_exclusive(waiter->lock);
 }
 
 void waiter_destroy(WAITER_HANDLE waiter)
@@ -120,9 +76,22 @@ void waiter_destroy(WAITER_HANDLE waiter)
     }
     else
     {
-        sm_close_begin_with_cb(waiter->sm, abandon, waiter, NULL, NULL);
+        srw_lock_acquire_exclusive(waiter->lock);
+        if (!waiter->callbacks_called)
+        {
+            if (waiter->registered)
+            {
+                waiter->notification_callback(waiter->notification_callback_context, NULL, WAITER_RESULT_ABANDONED);
+            }
+            if (waiter->notified)
+            {
+                waiter->notify_complete_callback(waiter->notify_complete_callback_context, WAITER_RESULT_ABANDONED);
+                THANDLE_ASSIGN(RC_PTR)(&waiter->data, NULL);
+            }
+            waiter->callbacks_called = true;
+        }
+        srw_lock_release_exclusive(waiter->lock);
         srw_lock_destroy(waiter->lock);
-        sm_destroy(waiter->sm);
         free(waiter);
     }
 }
@@ -134,8 +103,6 @@ static void cancel_register_notification(void* context)
     {
         waiter->notification_callback(waiter->notification_callback_context, NULL, WAITER_RESULT_CANCELLED);
         waiter->callbacks_called = true;
-        sm_fault(waiter->sm);
-        sm_exec_end(waiter->sm);
     }
     srw_lock_release_exclusive(waiter->lock);
 }
@@ -149,13 +116,10 @@ static void try_complete(WAITER_HANDLE waiter)
 {
     if (waiter->registered && waiter->notified && !waiter->callbacks_called)
     {
-        sm_fault(waiter->sm);
-        waiter->callbacks_called = true;
-        sm_exec_end(waiter->sm);
         waiter->notification_callback(waiter->notification_callback_context, waiter->data, WAITER_RESULT_OK);
         THANDLE_ASSIGN(RC_PTR)(&waiter->data, NULL);
-        sm_exec_end(waiter->sm);
         waiter->notify_complete_callback(waiter->notify_complete_callback_context, WAITER_RESULT_OK);
+        waiter->callbacks_called = true;
     }
 }
 
@@ -169,62 +133,39 @@ THANDLE(ASYNC_OP) waiter_register_notification(WAITER_HANDLE waiter, NOTIFICATIO
     }
     else
     {
-        SM_RESULT sm_exec_begin_result = sm_exec_begin(waiter->sm);
-        if (sm_exec_begin_result != SM_EXEC_GRANTED)
+        srw_lock_acquire_exclusive(waiter->lock);
         {
-            LogError("Failure in sm_exec_begin(waiter->sm=%p)", waiter->sm);
-        }
-        else
-        {
-            bool success;
-            srw_lock_acquire_exclusive(waiter->lock);
+            if (waiter->registered)
             {
-                if (waiter->registered)
-                {
-                    LogError(
-                        "waiter_register_notification called twice on WAITER waiter=%p."
-                        " Previous arguments: NOTIFICATION_CALLBACK notification_callback=%p, void* context=%p"
-                        " Current arguments: NOTIFICATION_CALLBACK notification_callback=%p, void* context=%p",
-                        waiter,
-                        waiter->notification_callback, waiter->notification_callback_context,
-                        notification_callback, context
-                    );
-                    success = false;
-                }
-                else
-                {
-                    THANDLE(ASYNC_OP) async_op = async_op_create(cancel_register_notification, sizeof(WAITER_HANDLE), alignof(WAITER_HANDLE), dispose);
-                    if (async_op == NULL)
-                    {
-                        LogError("Failure in async_op_create(cancel_register_notification=%p, sizeof(WAITER_HANDLE)=%" PRIu32 ", alignof(WAITER_HANDLE)=%" PRIu32 ", dispose=%p)", cancel_register_notification, (uint32_t)sizeof(WAITER_HANDLE), (uint32_t)alignof(WAITER_HANDLE), dispose);
-                        success = false;
-                    }
-                    else
-                    {
-                        ((ASYNC_OP*)async_op)->context = waiter;
-                        waiter->notification_callback = notification_callback;
-                        waiter->notification_callback_context = context;
-                        waiter->registered = true;
-                        try_complete(waiter);
-                        THANDLE_INITIALIZE_MOVE(ASYNC_OP)(&result, &async_op);
-                        success = true;
-                    }
-                }
-            }
-            srw_lock_release_exclusive(waiter->lock);
-
-            if (!success)
-            {
-                LogError("Failure in waiter_register_notification(waiter=%p, notification_callback=%p, context=%p)", waiter, notification_callback, context);
+                LogError(
+                    "waiter_register_notification called twice on WAITER waiter=%p."
+                    " Previous arguments: NOTIFICATION_CALLBACK notification_callback=%p, void* context=%p"
+                    " Current arguments: NOTIFICATION_CALLBACK notification_callback=%p, void* context=%p",
+                    waiter,
+                    waiter->notification_callback, waiter->notification_callback_context,
+                    notification_callback, context
+                );
             }
             else
             {
-                goto all_ok;
+                THANDLE(ASYNC_OP) async_op = async_op_create(cancel_register_notification, sizeof(WAITER_HANDLE), alignof(WAITER_HANDLE), dispose);
+                if (async_op == NULL)
+                {
+                    LogError("Failure in async_op_create(cancel_register_notification=%p, sizeof(WAITER_HANDLE)=%" PRIu32 ", alignof(WAITER_HANDLE)=%" PRIu32 ", dispose=%p)", cancel_register_notification, (uint32_t)sizeof(WAITER_HANDLE), (uint32_t)alignof(WAITER_HANDLE), dispose);
+                }
+                else
+                {
+                    ((ASYNC_OP*)async_op)->context = waiter;
+                    waiter->notification_callback = notification_callback;
+                    waiter->notification_callback_context = context;
+                    waiter->registered = true;
+                    try_complete(waiter);
+                    THANDLE_INITIALIZE_MOVE(ASYNC_OP)(&result, &async_op);
+                }
             }
-            sm_exec_end(waiter->sm);
         }
+        srw_lock_release_exclusive(waiter->lock);
     }
-all_ok:
     return result;
 }
 
@@ -236,8 +177,6 @@ static void cancel_notify(void* context)
         waiter->notify_complete_callback(waiter->notify_complete_callback_context, WAITER_RESULT_CANCELLED);
         waiter->callbacks_called = true;
         THANDLE_ASSIGN(RC_PTR)(&waiter->data, NULL);
-        sm_fault(waiter->sm);
-        sm_exec_end(waiter->sm);
     }
     srw_lock_release_exclusive(waiter->lock);
 }
@@ -252,61 +191,39 @@ THANDLE(ASYNC_OP) waiter_notify(WAITER_HANDLE waiter, THANDLE(RC_PTR) data, NOTI
     }
     else
     {
-        SM_RESULT sm_exec_begin_result = sm_exec_begin(waiter->sm);
-        if (sm_exec_begin_result != SM_EXEC_GRANTED)
+        srw_lock_acquire_exclusive(waiter->lock);
         {
-            LogError("Failure in sm_exec_begin(waiter->sm=%p)", waiter->sm);
-        }
-        else
-        {
-            bool success;
-            srw_lock_acquire_exclusive(waiter->lock);
+            if (waiter->notified)
             {
-                if (waiter->notified)
-                {
-                    LogError("waiter_notify called twice on WAITER waiter=%p."
-                        "Previous arguments: THANDLE(RC_PTR) data=%" PRI_RC_PTR ", NOTIFY_COMPLETE_CALLBACK notify_complete_callback=%p, void* context=%p."
-                        "Current arguments: THANDLE(RC_PTR) data=%" PRI_RC_PTR ", NOTIFY_COMPLETE_CALLBACK notify_complete_callback=%p, void* context=%p.",
-                        waiter,
-                        waiter->data, waiter->notify_complete_callback, waiter->notify_complete_callback_context,
-                        data, notify_complete_callback, context
-                    );
-                    success = false;
-                }
-                else
-                {
-                    THANDLE(ASYNC_OP) async_op = async_op_create(cancel_notify, sizeof(WAITER_HANDLE), alignof(WAITER_HANDLE), dispose);
-                    if (async_op == NULL)
-                    {
-                        LogError("Failure in async_op_create(cancel_notify=%p, sizeof(WAITER_HANDLE)=%" PRIu32 ", alignof(WAITER_HANDLE)=%" PRIu32 ", dispose=%p)", cancel_notify, (uint32_t)sizeof(WAITER_HANDLE), (uint32_t)alignof(WAITER_HANDLE), dispose);
-                        success = false;
-                    }
-                    else
-                    {
-                        ((ASYNC_OP*)async_op)->context = waiter;
-
-                        THANDLE_ASSIGN(RC_PTR)(&waiter->data, data);
-                        waiter->notify_complete_callback = notify_complete_callback;
-                        waiter->notify_complete_callback_context = context;
-                        waiter->notified = true;
-                        try_complete(waiter);
-                        THANDLE_INITIALIZE_MOVE(ASYNC_OP)(&result, &async_op);
-                        success = true;
-                    }
-                }
-            }
-            srw_lock_release_exclusive(waiter->lock);
-            if (!success)
-            {
-                LogError("Failure in waiter_notify(waiter=%p, data=%" PRI_RC_PTR ", notify_complete_callback=%p, context=%p)", waiter, data, notify_complete_callback, context);
+                LogError("waiter_notify called twice on WAITER waiter=%p."
+                    "Previous arguments: THANDLE(RC_PTR) data=%" PRI_RC_PTR ", NOTIFY_COMPLETE_CALLBACK notify_complete_callback=%p, void* context=%p."
+                    "Current arguments: THANDLE(RC_PTR) data=%" PRI_RC_PTR ", NOTIFY_COMPLETE_CALLBACK notify_complete_callback=%p, void* context=%p.",
+                    waiter,
+                    waiter->data, waiter->notify_complete_callback, waiter->notify_complete_callback_context,
+                    data, notify_complete_callback, context
+                );
             }
             else
             {
-                goto all_ok;
+                THANDLE(ASYNC_OP) async_op = async_op_create(cancel_notify, sizeof(WAITER_HANDLE), alignof(WAITER_HANDLE), dispose);
+                if (async_op == NULL)
+                {
+                    LogError("Failure in async_op_create(cancel_notify=%p, sizeof(WAITER_HANDLE)=%" PRIu32 ", alignof(WAITER_HANDLE)=%" PRIu32 ", dispose=%p)", cancel_notify, (uint32_t)sizeof(WAITER_HANDLE), (uint32_t)alignof(WAITER_HANDLE), dispose);
+                }
+                else
+                {
+                    ((ASYNC_OP*)async_op)->context = waiter;
+
+                    THANDLE_ASSIGN(RC_PTR)(&waiter->data, data);
+                    waiter->notify_complete_callback = notify_complete_callback;
+                    waiter->notify_complete_callback_context = context;
+                    waiter->notified = true;
+                    try_complete(waiter);
+                    THANDLE_INITIALIZE_MOVE(ASYNC_OP)(&result, &async_op);
+                }
             }
-            sm_exec_end(waiter->sm);
         }
+        srw_lock_release_exclusive(waiter->lock);
     }
-all_ok:
     return result;
 }
