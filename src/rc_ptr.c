8154b3f7daaff74361e404860d65fdeca86367ce
Parth Aggarwal <paaggarwal@microsoft.com>
2023-05-24 19:07:21 -0700

    removed code

--- src/rc_ptr.c
+++ src/rc_ptr.c
@@ -16,35 +16,10 @@
 
 THANDLE_TYPE_DEFINE(RC_PTR);
 
-static void dispose(RC_PTR* rc_ptr)
-{
-    if (rc_ptr->free_func != NULL)
-    {
-        rc_ptr->free_func((void*)rc_ptr->ptr);
-    }
-}
 
 THANDLE(RC_PTR) rc_ptr_create_with_move_pointer(void* ptr, RC_PTR_FREE_FUNC free_func)
 {
-    THANDLE(RC_PTR) result = NULL;
-    if (ptr == NULL)
-    {
-        LogError("Invalid arguments: void* ptr=%p, RC_PTR_FREE_FUNC free_func = %p", ptr, free_func);
-    }
-    else
-    {
-        THANDLE(RC_PTR) thandle = THANDLE_MALLOC(RC_PTR)(dispose);
-        if (thandle == NULL)
-        {
-            LogError("Failure in THANDLE_MALLOC(RC_PTR)(dispose=%p)", dispose);
-        }
-        else
-        {
-            RC_PTR* rc_ptr = THANDLE_GET_T(RC_PTR)(thandle);
-            rc_ptr->ptr = ptr;
-            rc_ptr->free_func = free_func;
-            THANDLE_INITIALIZE_MOVE(RC_PTR)(&result, &thandle);
-        }
-    }
-    return result;
+    (void)ptr;
+    (void)free_func;
+    return NULL;
 }
