04516f92afa6c0ac20ce666211e6e347334a6e1b
Dan Cristoloveanu <dcristo@microsoft.com>
2023-11-03 13:46:02 -0700

    Fix worker_thread reals (#323)

--- tests/reals/real_worker_thread.c
+++ tests/reals/real_worker_thread.c
@@ -3,6 +3,8 @@
 #include "real_gballoc_hl_renames.h" // IWYU pragma: keep
 #include "real_threadapi_renames.h" // IWYU pragma: keep
 #include "real_sm_renames.h" // IWYU pragma: keep
+#include "real_interlocked_hl_renames.h" // IWYU pragma: keep
+#include "real_sync_renames.h" // IWYU pragma: keep
 
 #include "real_worker_thread_renames.h" // IWYU pragma: keep
 
