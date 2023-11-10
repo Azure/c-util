fa541ba70030565a8f9eea92cea0b1c5c0152cc8
Dan Cristoloveanu <dcristo@microsoft.com>
2023-11-10 14:24:51 -0800

    Fix reals for tcall_dispatcher_thread_notification_call (#329)

--- tests/reals/real_tcall_dispatcher_thread_notification_call_renames.h
+++ tests/reals/real_tcall_dispatcher_thread_notification_call_renames.h
@@ -3,5 +3,6 @@
 
 #include "real_srw_lock_ll_renames.h"
 #include "real_doublylinkedlist_renames.h"
+#include "real_interlocked_renames.h"
 
 #define THREAD_NOTIFICATION_CALL real_THREAD_NOTIFICATION_CALL
