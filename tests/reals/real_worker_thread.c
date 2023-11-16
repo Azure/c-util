f5564dc1c28f1a026ad1e0882c99259e011bc1b6
Matt Durak <mdurak@microsoft.com>
2023-11-16 10:13:51 -0800

    Fix real_worker_thread to not have mock interlocked calls (#331)

--- tests/reals/real_worker_thread.c
+++ tests/reals/real_worker_thread.c
@@ -4,6 +4,7 @@
 #include "real_threadapi_renames.h" // IWYU pragma: keep
 #include "real_sm_renames.h" // IWYU pragma: keep
 #include "real_interlocked_hl_renames.h" // IWYU pragma: keep
+#include "real_interlocked_renames.h" // IWYU pragma: keep
 #include "real_sync_renames.h" // IWYU pragma: keep
 
 #include "real_worker_thread_renames.h" // IWYU pragma: keep
