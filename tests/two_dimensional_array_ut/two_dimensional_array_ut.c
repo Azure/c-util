0d4f25cb031aff8c4fdf26caf27942042d4db983
Bingyi Yang <bingyiyang@microsoft.com>
2024-01-16 10:23:53 -0800

    dix build err

---
+++ tests/two_dimensional_array_ut/two_dimensional_array_ut.c
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include "macro_utils/macro_utils.h"
+#include "testrunnerswitcher.h"
+#include "umock_c/umock_c.h"
+#include "umock_c/umocktypes_charptr.h"
+#include "umock_c/umock_c_negative_tests.h"
+
+#define ENABLE_MOCKS
+#include "c_pal/gballoc_hl.h"
+#include "c_pal/gballoc_hl_redirect.h"
+#include "c_pal/uuid.h"
+#undef ENABLE_MOCKS
+
+
+#include "real_uuid.h" /*the one from c_pal*/
+#include "real_gballoc_hl.h"
+
+#include "c_util/uuid_string.h"
