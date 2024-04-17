f8d23b92919df1025f36e25b53df094d778f1beb
Dan Cristoloveanu <dcristo@microsoft.com>
2024-04-16 18:09:48 -0700

    Fix cancellation token reals

--- tests/reals/real_cancellation_token.c
+++ tests/reals/real_cancellation_token.c
@@ -4,6 +4,7 @@
 #include "real_gballoc_hl_renames.h"
 #include "c_pal/gballoc_hl_redirect.h"
 
+#include "real_tcall_dispatcher_cancellation_token_cancel_call_renames.h"
 #include "real_cancellation_token_renames.h"
 
 #include "../../src/cancellation_token.c"
