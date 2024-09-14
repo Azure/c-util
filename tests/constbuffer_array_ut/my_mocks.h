3ae748b4a3d89850bc41b9c011afcb27db1bd23f
Andrei Porumb <anporumb@microsoft.com>
2024-09-14 10:58:26 -0700

    propagate_all_repos anporumb/pchmock

---
+++ tests/constbuffer_array_ut/my_mocks.h
@@ -0,0 +1,11 @@
+#ifndef my_mocks_h
+#define my_mocks_h
+
+#define ENABLE_MOCKS
+#include "c_pal/gballoc_hl.h"
+#include "c_pal/gballoc_hl_redirect.h"
+#include "c_pal/interlocked.h"
+#include "c_util/constbuffer.h"
+#undef ENABLE_MOCKS
+
+#endif
