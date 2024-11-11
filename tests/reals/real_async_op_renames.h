e0b1bbb4b639584c0c48871d648ff2cbfcfd3491
Jelani Brandon <jelani.brandon@microsoft.com>
2024-11-11 13:32:25 -0800

    Fixing the Async OP real file (#448)
    Co-authored-by: Jelani Brandon <jebrando@microsoft.com>

--- tests/reals/real_async_op_renames.h
+++ tests/reals/real_async_op_renames.h
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-#define ASYNC_OP real_ASYNC_OP
-#define async_op_create real_async_op_create
-#define async_op_cancel real_async_op_cancel
-#define ASYNC_OP_STATE real_ASYNC_OP_STATE
+#define ASYNC_OP            real_ASYNC_OP
+
+#define async_op_create     real_async_op_create
+#define async_op_cancel     real_async_op_cancel
+
+#define ASYNC_OP_STATE      real_ASYNC_OP_STATE
