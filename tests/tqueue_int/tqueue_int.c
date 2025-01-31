4078bc78c4b34cf8620c7be8f70cabc73d601ab0
Nishikant Deshmukh <nisdeshmukh@microsoft.com>
2025-01-30 20:34:15 -0800

    Reverted InterlockedHL_WaitForNotValue

--- tests/tqueue_int/tqueue_int.c
+++ tests/tqueue_int/tqueue_int.c
@@ -13,8 +13,6 @@
 #include "c_pal/gballoc_hl_redirect.h"
 
 #include "c_pal/interlocked.h"
-#include "c_pal/interlocked_hl.h"
-#include "c_pal/sync.h"
 #include "c_pal/thandle.h"
 #include "c_pal/threadapi.h"
 #include "c_pal/timer.h"
@@ -344,7 +342,6 @@ static int pusher_thread_func(void* arg)
         if (push_result == TQUEUE_PUSH_OK)
         {
             (void)interlocked_increment_64(&test_context->successful_push_count);
-            wake_by_address_single_64(&test_context->successful_push_count);
         }
         THANDLE_ASSIGN(TEST_THANDLE)(&item, NULL);
 
@@ -373,7 +370,6 @@ static int popper_thread_func(void* arg)
         if (pop_result == TQUEUE_POP_OK)
         {
             (void)interlocked_increment_64(&test_context->successful_pop_count);
-            wake_by_address_single_64(&test_context->successful_pop_count);
             THANDLE_ASSIGN(TEST_THANDLE)(&item, NULL);
         }
 
@@ -397,11 +393,6 @@ static void test_and_terminate_chaos_test(TQUEUE_CHAOS_TEST_THANDLE_CONTEXT *tes
         // get how many pushes and pops at the beginning of the time slice
         int64_t last_successful_push_count = interlocked_add_64(&test_context->successful_push_count, 0);
         int64_t last_successful_pop_count = interlocked_add_64(&test_context->successful_pop_count, 0);
-
-        // Ensures that the tests below for current_successful_push_count > last_successful_push_count and/or current_successful_pop_count > last_successful_pop_count
-        // do not fail due to multi-threading synchronization issues
-        InterlockedHL_WaitForNotValue64(&test_context->successful_push_count, last_successful_push_count, INT_MAX);
-        InterlockedHL_WaitForNotValue64(&test_context->successful_pop_count, last_successful_pop_count, INT_MAX);
 
         ThreadAPI_Sleep(TEST_CHECK_PERIOD);
 
@@ -535,7 +526,6 @@ static int tqueue_chaos_thread_THANDLE_func(void* arg)
             if (push_result == TQUEUE_PUSH_OK)
             {
                 (void)interlocked_increment_64(&test_context->successful_push_count);
-                wake_by_address_single_64(&test_context->successful_push_count);
             }
             THANDLE_ASSIGN(TEST_THANDLE)(&item, NULL);
             break;
@@ -549,7 +539,6 @@ static int tqueue_chaos_thread_THANDLE_func(void* arg)
             {
                 ASSERT_ARE_NOT_EQUAL(int64_t, -1, item->a_value);
                 (void)interlocked_increment_64(&test_context->successful_pop_count);
-                wake_by_address_single_64(&test_context->successful_pop_count);
                 THANDLE_ASSIGN(TEST_THANDLE)(&item, NULL);
             }
             break;
@@ -563,7 +552,6 @@ static int tqueue_chaos_thread_THANDLE_func(void* arg)
             {
                 ASSERT_ARE_NOT_EQUAL(int64_t, -1, item->a_value);
                 (void)interlocked_increment_64(&test_context->successful_pop_count);
-                wake_by_address_single_64(&test_context->successful_pop_count);
                 THANDLE_ASSIGN(TEST_THANDLE)(&item, NULL);
             }
             break;
@@ -573,7 +561,6 @@ static int tqueue_chaos_thread_THANDLE_func(void* arg)
             int64_t current_count = TQUEUE_GET_VOLATILE_COUNT(THANDLE(TEST_THANDLE))(test_context->queue);
             ASSERT_IS_TRUE(((current_count >= 0) && (current_count <= test_context->max_queue_size)));
             (void)interlocked_increment_64(&test_context->successful_get_volatile_count);
-            wake_by_address_single_64(&test_context->successful_pop_count);
         }
         }
         current_time = timer_global_get_elapsed_ms();
