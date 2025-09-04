21355ba7f224fc35b737b60421706d83d2c66c65
Dan Cristoloveanu <dcristo@microsoft.com>
2025-09-04 06:15:45 -0700

    Fix channel reals (#525)

--- tests/reals/real_channel.c
+++ tests/reals/real_channel.c
@@ -7,7 +7,9 @@
 #include "real_doublylinkedlist_renames.h"
 #include "real_async_op_renames.h"
 #include "real_rc_ptr_renames.h"
+#include "real_rc_string_renames.h"
 #include "real_sm_renames.h"
+#include "real_thandle_log_context_handle_renames.h"
 
 #include "real_channel_renames.h"
 
