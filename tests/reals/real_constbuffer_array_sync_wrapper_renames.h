88090a603357ef9d66883e683181fc5b460cb1e6
Bert Kleewein <bertk@microsoft.com>
2026-01-22 08:20:45 -0800

    Add reals for constbuffer_array_sync_wrapepr

---
+++ tests/reals/real_constbuffer_array_sync_wrapper_renames.h
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef REAL_CONSTBUFFER_ARRAY_SYNC_WRAPPER_RENAMES_H
+#define REAL_CONSTBUFFER_ARRAY_SYNC_WRAPPER_RENAMES_H
+
+#define async_type_helper_CONSTBUFFER_ARRAY_HANDLE_copy real_async_type_helper_CONSTBUFFER_ARRAY_HANDLE_copy
+#define async_type_helper_CONSTBUFFER_ARRAY_HANDLE_free real_async_type_helper_CONSTBUFFER_ARRAY_HANDLE_free
+
+#endif // REAL_CONSTBUFFER_ARRAY_SYNC_WRAPPER_RENAMES_H
